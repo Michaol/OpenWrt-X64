c6d903972544118ca5806f2869a13115abd5d616
Michaol <michaol@gmail.com>
2020-07-03 09:01:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/jerrykuku/node-request.git package/lean
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean
+git clone https://github.com/jerrykuku/node-request.git package/lean/
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/
