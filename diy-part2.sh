df794d1fc5dd02022b0152b56619a62e59d52ddb
Michaol <michaol@gmail.com>
2020-09-21 16:53:07 +0800

    Add OpenClash

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Add OpenClash
+# Add OpenClash test #1
 git clone -b master https://github.com/vernesong/OpenClash package/openclash
